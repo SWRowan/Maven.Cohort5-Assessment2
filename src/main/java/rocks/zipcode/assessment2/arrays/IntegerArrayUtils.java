99da71240c6cc3932381c05d478ae6a3990cd9c2
Sean Rowan <seanrowan@zipcoders-MacBook-Pro.local>
2019-03-04 09:33:10 -0500

    Integer Array Utils complete

--- src/main/java/rocks/zipcode/assessment2/arrays/IntegerArrayUtils.java
+++ src/main/java/rocks/zipcode/assessment2/arrays/IntegerArrayUtils.java
@@ -1,5 +1,9 @@
 package rocks.zipcode.assessment2.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author leon on 28/11/2018.
  */
@@ -10,7 +14,12 @@ public class IntegerArrayUtils {
      * @return - identical array with one additional element of `valueToBeAdded` at the end of the array
      */
     public static Integer[] add(Integer[] integerArray, Integer valueToBeAdded) {
-        return null;
+        List<Integer> list = new ArrayList<>(Arrays.asList(integerArray));
+        list.add(valueToBeAdded);
+        Integer[] answer = new Integer[list.size()];
+        answer = list.toArray(answer);
+
+        return answer;
     }
 
     /**
@@ -20,7 +29,13 @@ public class IntegerArrayUtils {
      * @return `integerArray` with `valueToBeInserted` at index number `indexToInsertAt`
      */
     public static Integer[] replace(Integer[] integerArray, int indexToInsertAt, Integer valueToBeInserted) {
-        return null;
+        List<Integer> list = new ArrayList<>(Arrays.asList(integerArray));
+        list.remove(indexToInsertAt);
+        list.add(indexToInsertAt, valueToBeInserted);
+        Integer[] answer = new Integer[list.size()];
+        answer = list.toArray(answer);
+
+        return answer;
     }
 
     /**
@@ -29,7 +44,9 @@ public class IntegerArrayUtils {
      * @return element located at `indexToFetch`
      */
     public static Integer get(Integer[] integerArray, Integer indexToFetch) {
-        return null;
+        List<Integer> list = new ArrayList<>(Arrays.asList(integerArray));
+        Integer answer = list.get(indexToFetch);
+        return answer;
     }
 
     /**
@@ -37,7 +54,20 @@ public class IntegerArrayUtils {
      * @return identical array with even-values incremented by 1 and odd-values decremented by 1
      */
     public static Integer[] incrementEvenDecrementOdd(Integer[] integerArray) {
-        return null;
+        List<Integer> list = new ArrayList<>(Arrays.asList(integerArray));
+        List<Integer> newList = new ArrayList<>();
+        for (Integer num : list) {
+            if(num % 2 == 0){
+                num++;
+                newList.add(num);
+            } else{
+                num--;
+                newList.add(num);
+            }
+        }
+        Integer[] answer = new Integer[integerArray.length];
+        answer = newList.toArray(answer);
+        return answer;
     }
 
     /**
@@ -45,7 +75,19 @@ public class IntegerArrayUtils {
      * @return identical array with even-values incremented by 1
      */
     public static Integer[] incrementEven(Integer[] integerArray) {
-        return null;
+        List<Integer> list = new ArrayList<>(Arrays.asList(integerArray));
+        List<Integer> newList = new ArrayList<>();
+        for (Integer num : list) {
+            if(num % 2 == 0) {
+                num++;
+                newList.add(num);
+            } else {
+            newList.add(num);
+            }
+        }
+        Integer[] answer = new Integer[integerArray.length];
+        answer = newList.toArray(answer);
+        return answer;
     }
 
     /**
@@ -53,6 +95,18 @@ public class IntegerArrayUtils {
      * @return identical array with odd-values decremented by 1
      */
     public static Integer[] decrementOdd(Integer[] input) {
-        return null;
+        List<Integer> list = new ArrayList<>(Arrays.asList(input));
+        List<Integer> newList = new ArrayList<>();
+        for (Integer num : list) {
+            if(num % 2 != 0) {
+                num--;
+                newList.add(num);
+            } else {
+                newList.add(num);
+            }
+        }
+        Integer[] answer = new Integer[input.length];
+        answer = newList.toArray(answer);
+        return answer;
     }
 }
